48f536368b515237c3b7823dd30737a04b184ed5
TobiasVandenBerckt <Tobias.vandenberckt@student.ucll.be>
2024-06-05 15:45:22 +0200

    profilecard

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -9,37 +9,49 @@ const Profile : React.FC<user> = () => {
   return (
     <>
     <Header></Header>
-    <div className="profile-body">
-      <h2>Biography:</h2>
-      <p>bio</p>
-      <h2>publicaties:</h2>
-      <ul>
-          <li>a</li>        
-          <li>b</li>  
-      </ul>
+    <div className="profile-container">
+      <div className="profile-left">
+        <div className="profile-bio">
+          <h2>Biografie:</h2>
+          <p>bio</p>
+        </div>
+        <div className="profile-publications">
+          <h2>publicaties:</h2>
+          <ul>
+            <li>publicatie1</li>
+            <li>publicatie2</li>
+            <li>publicatie3</li>
+          </ul>
+        </div>
+      </div>
+      <div className="profile-info">
+        <h2>Bob De Bouwer</h2>
+        <p>Fotoke</p>
+        <p>KUL</p>
+        <ul>
+          <li>Dokter</li>
+          <li>Dokter2</li>
+        </ul>
+        <p>bob@bob.be</p>
+        <p>telefoon</p>
+        <ul>
+          <li>a</li>
+          <li>b</li>
+        </ul>
+        <ul>
+          <li>a</li>
+          <li>b</li>
+        </ul>
+        <p>KUL</p>
+        <p>urbanlab gerelateerd: ja</p>
+        <p>link</p>
+      </div>
     </div>
-    <div className="profile-info">
-      <h2>Bob De Bouwer</h2>
-      <p>Fotoke</p>
-      <p>KUL</p>
-      <ul>
-        <li>Dokter</li>
-        <li>Dokter2</li>
-      </ul>
-      <p>bob@bob.be</p>
-      <p>telefoon</p>
-      <ul>
-        <li>a</li>
-        <li>b</li>
-      </ul>
-      <ul>
-        <li>a</li>
-        <li>b</li>
-      </ul>
-      <p>KUL</p>
-      <p>urbanlab gerelateerd: ja</p>
-      <p>link</p>
-    </div>
+
+
+  
+  
+    
     </>
   );
 };
