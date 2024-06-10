a3364e698594b6746e201dc42cbc5d9b792fcf03
TobiasVandenBerckt <Tobias.vandenberckt@student.ucll.be>
2024-06-10 13:49:41 +0200

    overview

--- components/userOverview.tsx
+++ components/userOverview.tsx
@@ -27,38 +27,38 @@ const UserOverviewTable: React.FC<Props> = ({ users }: Props) => {
     };
     return (
         <>
-            {users && users.map((user, index) => (
-                <div key={index} className="user-window" style={{ border: '1px solid #ccc', backgroundColor: '#f9f9f9', padding: '10px', marginBottom: '10px' }}>
-                    <div className="w-100 d-none d-md-block" />
-                    <div className="col-6">
-                        <table className="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Naam</th>
-                                    <th scope="col">Functie</th>
-                                    <th scope="col">Skill</th>
-                                    <th scope="col">Thema</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td style={{ borderRight: '1px solid #ccc', verticalAlign: 'middle', textAlign: 'center' }}>
-                                        <button style={{ background: 'none', border: 'none', color: 'blue', textDecoration: 'underline', cursor: 'pointer' }} onClick={() => handleUserClick(user.voornaam, user.achternaam, user.huidige_functie, user.skills, user.themas)}>
-                                            {user.voornaam} {user.achternaam}
-                                        </button>
-                                    </td>
-                                    <td style={{ borderRight: '1px solid #ccc' }}>{user.huidige_functie}</td>
-                                    <td style={{ borderRight: '1px solid #ccc' }}>{user.skills}</td>
-                                    <td>{user.themas.map((thema, index) => (
-                                        <span key={index}>{thema}{index < user.themas.length - 1 && ', '}</span>
-                                    ))}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            ))}
-        </>
+        {users && users.map((user, index) => (
+          <div key={index} className="user-window" style={{ border: '1px solid #ccc', backgroundColor: '#f9f9f9', padding: '10px', marginBottom: '10px' }}>
+            <div className="w-100 d-none d-md-block" />
+            <div className="col-6">
+              <table className="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">Naam</th>
+                    <th scope="col">Functie</th>
+                    <th scope="col">Skill</th>
+                    <th scope="col">Thema</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className="fixed-width" style={{ verticalAlign: 'middle', textAlign: 'center' }}>
+                      <button style={{ background: 'none', border: 'none', color: 'blue', textDecoration: 'underline', cursor: 'pointer' }} onClick={() => handleUserClick(user.voornaam, user.achternaam, user.huidige_functie, user.skills, user.themas)}>
+                        {user.voornaam} {user.achternaam}
+                      </button>
+                    </td>
+                    <td className="fixed-width">{user.huidige_functie}</td>
+                    <td className="fixed-width">{user.skills}</td>
+                    <td className="fixed-width">{user.themas.map((thema, index) => (
+                      <span key={index}>{thema}{index < user.themas.length - 1 && ', '}</span>
+                    ))}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        ))}
+      </>
     )
 }
 
